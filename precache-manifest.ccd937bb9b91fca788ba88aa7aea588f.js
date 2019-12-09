3d4923d92801a3b298cdb16ab4f55ade8e5f36af
sylvia326 <hxiaowei@uwo.ca>
2019-12-09 13:40:05 -0500

    Updates

---
+++ precache-manifest.ccd937bb9b91fca788ba88aa7aea588f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c9495c1b4e2f03e912f67d2cb5627ff",
+    "url": "/weather-finder/index.html"
+  },
+  {
+    "revision": "ddfe646fff298c2f179d",
+    "url": "/weather-finder/static/css/2.fe6c84aa.chunk.css"
+  },
+  {
+    "revision": "44f7e2aeafab2c70cd7b",
+    "url": "/weather-finder/static/css/main.ca8df08a.chunk.css"
+  },
+  {
+    "revision": "ddfe646fff298c2f179d",
+    "url": "/weather-finder/static/js/2.7032a2d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-finder/static/js/2.7032a2d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44f7e2aeafab2c70cd7b",
+    "url": "/weather-finder/static/js/main.bb3405dc.chunk.js"
+  },
+  {
+    "revision": "f7a54d6207874e5136f0",
+    "url": "/weather-finder/static/js/runtime-main.ab49c735.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/weather-finder/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/weather-finder/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/weather-finder/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/weather-finder/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/weather-finder/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
